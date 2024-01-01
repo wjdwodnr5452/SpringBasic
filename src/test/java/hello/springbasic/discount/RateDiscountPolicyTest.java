e7806b9b27fa286d21985427e80135d35c1b4911
정재욱 <wjdwodnr5452@naver.com>
2024-01-01 21:29:17 +0900

    새로운 할인 정책 개발

---
+++ src/test/java/hello/springbasic/discount/RateDiscountPolicyTest.java
@@ -0,0 +1,47 @@
+package hello.springbasic.discount;
+
+import hello.springbasic.member.Grade;
+import hello.springbasic.member.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class RateDiscountPolicyTest {
+
+
+    RateDiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+
+    @Test
+    @DisplayName("VIP는 10% 할인이 적용되어야 한다")
+    void vip_o(){
+        // given
+        Member member = new Member(1L, "memberVIP", Grade.VIP);
+
+        // when
+        int discount = discountPolicy.discount(member, 10000);
+
+        // then
+        assertThat(discount).isEqualTo(1000);
+
+    }
+    // 성공테스트도 중요하지만 실패 테스트도 만들어야함
+    @Test
+    @DisplayName("VIP가 아니면 할인이 적용되지 않아야 한다")
+    void vip_x(){
+
+        // given
+        Member member = new Member(1L, "memberBASIC", Grade.BASIC);
+
+        // when
+        int discount = discountPolicy.discount(member, 10000);
+
+        // then
+        assertThat(discount).isEqualTo(1000);
+
+    }
+
+}
