0a4f7934caa03262325ddab79722e5367965df1b
정재욱 <wjdwodnr5452@naver.com>
2024-01-04 22:53:23 +0900

    스프링 컨테이너 생성

--- src/main/java/hello/springbasic/AppConfig.java
+++ src/main/java/hello/springbasic/AppConfig.java
@@ -9,21 +9,29 @@ import hello.springbasic.member.MemberServiceImpl;
 import hello.springbasic.member.MemoryMemberRepository;
 import hello.springbasic.order.OrderService;
 import hello.springbasic.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration // 설정 정보, 구성정보를 담당
 public class AppConfig {
 
+    // @Bean을 사용 하면 스프링 컨테이너에 등록 됨
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
         //return new FixDiscountPolicy();
         return new RateDiscountPolicy();
--- src/main/java/hello/springbasic/MemberApp.java
+++ src/main/java/hello/springbasic/MemberApp.java
@@ -3,13 +3,18 @@ package hello.springbasic;
 import hello.springbasic.member.Grade;
 import hello.springbasic.member.Member;
 import hello.springbasic.member.MemberService;
-import hello.springbasic.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+        //AppConfig appConfig = new AppConfig();
+        //MemberService memberService = appConfig.memberService();
+
+        // 스프링은 ApplicationContext 으로 시작한다 이게 이제 스프링 컨테이너라고 보면된다. 빈들을 관리
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);// 인자 첫 번째는 이름이 들어옴 두 번째는 반환 타입이 들어옴 
 
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
