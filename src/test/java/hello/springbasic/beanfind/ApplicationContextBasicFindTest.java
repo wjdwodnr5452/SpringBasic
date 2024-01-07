ae0caa233a2e005df55a6f69c57461eb2b9fe132
정재욱 <wjdwodnr5452@naver.com>
2024-01-07 23:05:38 +0900

    스프링 빈 조회 - 기본

---
+++ src/test/java/hello/springbasic/beanfind/ApplicationContextBasicFindTest.java
@@ -0,0 +1,54 @@
+package hello.springbasic.beanfind;
+
+import hello.springbasic.AppConfig;
+import hello.springbasic.member.MemberService;
+
+import hello.springbasic.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ApplicationContextBasicFindTest {
+
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("빈 이름으로 조회")
+    void findBeanByName() {
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+
+
+    @Test
+    @DisplayName("이름 없이 타입으로만 조회")
+    void findBeanByType() {
+        MemberService memberService = ac.getBean(MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+
+
+    @Test
+    @DisplayName("구체 타입으로 조회")
+    void findBeanByName2() {
+        MemberService memberService = ac.getBean("memberService", MemberServiceImpl.class); // 구체로 가능 하지만 역할과 구현이 구별이 되어 있기 때문에 별로 안좋은 코드다
+        assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+
+    @Test
+    @DisplayName("빈 이름으로 조회x")
+    void findBeanByNameX() {
+       // MemberService xxxxxx = ac.getBean("xxxxxx", MemberServiceImpl.class);
+        assertThrows(NoSuchBeanDefinitionException.class, () -> ac.getBean("xxxxxx", MemberServiceImpl.class));
+    }
+
+
+
+
+}
