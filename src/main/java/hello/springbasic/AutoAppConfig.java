1201b668ffac8246d9eb53345e1a9a82ace7f2a7
정재욱 <wjdwodnr5452@naver.com>
2024-01-13 16:10:55 +0900

    담색위치와 기본 스캔 대상

--- src/main/java/hello/springbasic/AutoAppConfig.java
+++ src/main/java/hello/springbasic/AutoAppConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        basePackages = "hello.springbasic.member", // 패키지 경로 설정 지정을 안하면 모든 자바 경로 파일들을 다 찾는다.
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class) // Configuration.class 어노테이션이 붙은 애는 뺄거다. 기존 예제 코드 때문
 )
 public class AutoAppConfig {
