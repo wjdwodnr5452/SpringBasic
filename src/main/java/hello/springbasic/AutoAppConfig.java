d4c08d6b49adba7bbda4b60de1012fcb7385f7c1
정재욱 <wjdwodnr5452@naver.com>
2024-01-15 23:07:49 +0900

    중복 등록과 충돌

--- src/main/java/hello/springbasic/AutoAppConfig.java
+++ src/main/java/hello/springbasic/AutoAppConfig.java
@@ -1,15 +1,22 @@
 package hello.springbasic;
 
+import hello.springbasic.member.MemberRepository;
+import hello.springbasic.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
-        basePackages = "hello.springbasic.member", // 패키지 경로 설정 지정을 안하면 모든 자바 경로 파일들을 다 찾는다.
+        basePackages = "hello.springbasic", // 패키지 경로 설정 지정을 안하면 모든 자바 경로 파일들을 다 찾는다.
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class) // Configuration.class 어노테이션이 붙은 애는 뺄거다. 기존 예제 코드 때문
 )
 public class AutoAppConfig {
 
+    @Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
 
 }
