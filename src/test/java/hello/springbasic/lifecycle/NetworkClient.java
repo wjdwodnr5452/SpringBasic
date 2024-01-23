33c54ad1d58947881cc0a348f548960999b3f763
정재욱 <wjdwodnr5452@naver.com>
2024-01-23 23:20:38 +0900

    에노테이션 @PostConstruct, @PreDestroy

--- src/test/java/hello/springbasic/lifecycle/NetworkClient.java
+++ src/test/java/hello/springbasic/lifecycle/NetworkClient.java
@@ -1,6 +1,8 @@
 package hello.springbasic.lifecycle;
 
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 
 public class NetworkClient{
 
@@ -29,13 +31,14 @@ public class NetworkClient{
         System.out.println("close : " + url);
     }
 
-
+    @PostConstruct
     public void init() throws Exception {
         System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
     }
 
+    @PreDestroy
     public void close() throws Exception {
         System.out.println("NetworkClient.close");
         disconnect();
